67a0c00d6c9ab6f9734255e586310277b9ad0f06
mosesebto <mosesebto@gmail.com>
2026-03-08 10:06:13 -0400

    Completing Assess

---
+++ index.py
@@ -0,0 +1,12 @@
+def sort(width, height, length, mass):
+    volume = width * height * length
+
+    bulky = volume >= 1000000 or max(width, height, length) >= 150
+    heavy = mass >= 20
+
+    if bulky and heavy:
+        return "REJECTED"
+    elif bulky or heavy:
+        return "SPECIAL"
+    else:
+        return "STANDARD"
